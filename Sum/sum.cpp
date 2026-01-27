85e472c7f1588f6ab976cba1ed5f567cd452970f
sergiosk06 <al446629@uji.es>
2026-01-27 12:15:21 +0100

    Version3
    (actualizada)

--- Sum/sum.cpp
+++ Sum/sum.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 using namespace std;
 
+// funcion para computar
+
+int computar_suma(int n) {
+
+	int sum = 0;
+	int x;
+
+	for (int i = 0; i < n; ++i) {
+		cout << "Escribe un numero: ";
+		cin >> x;
+		sum += x;
+
+	}
+
+	return sum
+}
+
 int main() {
-    int n;
-    cout << "¿Cuantos numeros?"
-    cin >> n;
+	int n;
+	cout << "¿Cuantos numeros?"
+	cin >> n;
 
-    int sum = 0;
-    int x;
-    
-    for (int i = 0; i < n; ++i) {
-    	cout << "Escribe un numero: ";
-    	cin >> x;
-    	sum += x;
-    
-    }
-
-    cout << "la suma es " << sum << endl;
-    return 0;
+	int suma = computar_suma(n);
+	cout << "la suma es " << sum << endl;
+	return 0;
 }
    
