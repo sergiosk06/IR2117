ac133ecd63d9e7c02080181ed845b7737552812f
sergiosk06 <al446629@uji.es>
2026-01-27 12:46:52 +0100

    version4

--- Sum/sum_ab.cpp
+++ Sum/sum_ab.cpp
@@ -2,16 +2,27 @@
 using namespace std;
 
 int main() {
-    int a, b;
+	int a, b;
 
-    cout << "Introduce (a): ";
-    cin >> a;
-    cout << "Introduce (b): ";
-    cin >> b;
+	do {
+	
+		cout << "Introduce (a) siendo a positivo: ";
+		cin >> a;
+		
+	} while (a < 1 );
+	
+	do {
+	
+		cout << "Introduce (b) siendo b positivo y >= que a: ";
+		cin >> b;
+		
+	} while (b < 1 and b <= a);
+	
+		
+	
+	int sum = a + b;
+	cout << "La suma es " << sum << endl;
+	
+	return 0;
 
-    int sum = a + b;
-
-    cout << "La suma es " << sum << endl;
-    return 0;
 }
-
