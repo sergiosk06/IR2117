2d982ece687061a5a9b2da183ec7780bf1d540df
sergiosk06 <al446629@uji.es>
2026-01-27 12:33:36 +0100

    Create sum_ab.cpp
    version1

---
+++ Sum/sum_ab.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Introduce (a): ";
+    cin >> a;
+    cout << "Introduce (b): ";
+    cin >> b;
+
+    int sum = a + b;
+
+    cout << "La suma es " << sum << endl;
+    return 0;
+}
+
