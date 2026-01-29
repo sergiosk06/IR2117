101fa238762bc5ca5b977095f57d3a45448bdfb8
Sergio <al446629@uji.es>
2026-01-29 13:14:58 +0100

    v3

--- Cuadratica/cuadratica.cpp
+++ Cuadratica/cuadratica.cpp
@@ -13,7 +13,14 @@ int main() {
 	cin >> c;
 
 	cout << "a = " << a << ", b = " << b << ", c = " << c << ", soluciones = "<< soluciones << endl;
+	
+	double d = b*b - 4*a*c;
+	cout << "Discriminante = " << d << endl;
 
+	if (d > 0) {
+	soluciones += 2;
+	cout << soluciones << endl;
+	}
 	return 0;
 }
 
