7b6fb7e825b1039b09db8471bd91acdbe720d26e
Sergio <al446629@uji.es>
2026-01-29 12:57:30 +0100

    v5
    Mejorar la salida del programa: escribir la ecuación en la forma «ax + b = 0» y explicar el resultado con un texto (por ejemplo, «La ecuación tiene una única solución: x = …»).

--- Solve your math Problem/v1.cpp
+++ Solve your math Problem/v1.cpp
@@ -8,18 +8,18 @@ int main(){
 	float a = 0;
 	float b = 0;
 
-	cout << "Escribe un valor para a en una eccuaicion lineal 'ax+b=0' ";
+	cout << "Escribe un valor para 'a' en una eccuaicion lineal 'ax+b=0' ";
 	cin >> a;
-	cout << "Ahora escribe un valor para b en una eccuaicion lineal 'ax+b=0' ";
+	cout << "Ahora escribe un valor para 'b' en una eccuaicion lineal 'ax+b=0' ";
 	cin >> b;
-	cout << "El valor de a es " << a <<" y el valor de b es " << b << endl;
+	cout << "El valor de 'a' es " << a <<" y el valor de 'b' es " << b << " quedando la equacion como "<< a <<"x+"<< b <<"=0"<<endl;
 	
 	if (a != 0) {
 		float x = -b / a;
-		cout << "La solución es x = " << x << endl;
+		cout << "Hay una unica solucion para 'x' y es = " << x << endl;
 	}
 	else if (b != 0){
-		cout << "No hay solucion" << endl;
+		cout << "No hay solucion para x" << endl;
 	} else {
 		cout << "x tiene infinitas soluciones" << endl;
 	}
