a58fe7fa132bf1de34049800fde572343f45b350
Sergio <al446629@uji.es>
2026-01-29 12:38:43 +0100

    v2
    Suponer que:
    
    a!= 0; calcular la única solución
    
    x= −b/a y mostrarla.

--- Solve your math Problem/v1.cpp
+++ Solve your math Problem/v1.cpp
@@ -5,15 +5,19 @@ using namespace std;
 
 int main(){
 
-	int a = 0;
-	int b = 0;
+	float a = 0;
+	float b = 0;
 
 	cout << "Escribe un valor para a en una eccuaicion lineal 'ax+b=0' ";
 	cin >> a;
 	cout << "Ahora escribe un valor para b en una eccuaicion lineal 'ax+b=0' ";
 	cin >> b;
-
 	cout << "El valor de a es " << a <<" y el valor de b es " << b << endl;
-
-	return 0;
+	
+	if (a != 0) {
+		float x = -b / a;
+		cout << "La solución es x = " << x << endl;
+	}
+	
+	return 0;	
 }
