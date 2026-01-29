88c2817709fa2144b2a7edf6e23f35c3109792d1
Sergio <al446629@uji.es>
2026-01-29 13:10:56 +0100

    v1 cuadratica

---
+++ Cuadratica/cuadratica.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double a, b, c;
+	int soluciones = 0;
+
+	cout << "Coeficiente a: ";
+	cin >> a;
+	cout << "Coeficiente b: ";
+	cin >> b;
+	cout << "Coeficiente c: ";
+	cin >> c;
+
+	cout << "a = " << a << ", b = " << b << ", c = " << c << ", soluciones = "<< soluciones << endl;
+
+	return 0;
+}
+
