f7819dcc51694fe3f56e266e869357ce9c977371
Sergio <al446629@uji.es>
2026-01-29 12:26:45 +0100

    v1
    Pedir al usuario a y b; mostrarlos por pantalla para comprobar que la entrada funcional.

---
+++ Solve your math Problem/v1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//Ecuacion lineal
+
+int main(){
+
+	int a = 0;
+	int b = 0;
+
+	cout << "Escribe un valor para a en una eccuaicion lineal 'ax+b=0' ";
+	cin >> a;
+	cout << "Ahora escribe un valor para b en una eccuaicion lineal 'ax+b=0' ";
+	cin >> b;
+
+	cout << "El valor de a es " << a <<" y el valor de b es " << b << endl;
+
+	return 0;
+}
