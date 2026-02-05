f6c48fa69f49eaf92203b9020710acd31762f385
Sergio Serrano <al446629@uji.es>
2026-02-05 12:53:45 +0100

    publish sum

--- ros2_ws/src/sum/src/sum.cpp
+++ ros2_ws/src/sum/src/sum.cpp
@@ -3,22 +3,27 @@
 #include <iostream>
 
 int sum;
+std::shared_ptr< rclcpp::Publisher<std_msgs::msg::Int32> > publisher;
 
-void topic_callback(const std_msgs::msg::Int32::SharedPtr msg){
-	sum += msg->data;
-	std::cout << sum << std::endl;
-	
-}
+void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
+{
+   sum += msg->data;
+   std_msgs::msg::Int32 out_msg;
+   out_msg.data = sum;
+   publisher->publish(out_msg);
+ }
 
+ int main(int argc, char * argv[])
+ {
+   sum = 0;
+   rclcpp::init(argc, argv);
+   auto node = rclcpp::Node::make_shared("sum");
+   auto subscription =
+     node->create_subscription<std_msgs::msg::Int32>("number", 10,
+                                                      topic_callback);
+   publisher = node->create_publisher<std_msgs::msg::Int32>("sum", 10);
+   rclcpp::spin(node);
 
-int main(int argc, char * argv[]){
-	sum = 0;
-	rclcpp::init(argc, argv);
-	auto node = rclcpp::Node::make_shared("sum");
-	auto subscription =
-		node->create_subscription<std_msgs::msg::Int32>("number", 10,
-		                                     topic_callback);
-	rclcpp::spin(node);
 	rclcpp::shutdown();
 	return 0;
 }
