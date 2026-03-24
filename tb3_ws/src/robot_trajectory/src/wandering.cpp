c0c25188e8ff0fd6bab35a6e1c4277dcf65334f4
Sergio <al446629@uji.es>
2026-03-24 12:14:09 +0100

    wandering - v7

--- tb3_ws/src/robot_trajectory/src/wandering.cpp
+++ tb3_ws/src/robot_trajectory/src/wandering.cpp
@@ -7,6 +7,32 @@
 
 using namespace std::chrono_literals;
 
+float min_range09 = std::numeric_limits<float>::max();
+float min_range3509 = std::numeric_limits<float>::max();
+
+void MinRanges(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+	// Calcular el mínimo de ranges[0..9]
+	min_range09 = std::numeric_limits<float>::max();
+	for (int i = 0; i < 10; ++i) {
+    	if (msg->ranges[i] < min_range09) {
+        	min_range09 = msg->ranges[i];
+    	}
+	}
+
+	// Calcular el mínimo de ranges[350..359]
+	min_range3509 = std::numeric_limits<float>::max();
+	for (int i = 350; i < 360; ++i) {
+    	if (msg->ranges[i] < min_range3509) {
+        	min_range3509 = msg->ranges[i];
+    	}
+	}
+
+	// Mostrar los mínimos en pantalla
+	std::cout << "Mínimo delantero: " << min_range09 << std::endl;
+	std::cout << "Mínimo trasero: " << min_range3509 << std::endl;
+}
+
+
 int main(int argc, char * argv[])
 {
 	rclcpp::init(argc, argv);
@@ -19,8 +45,12 @@ int main(int argc, char * argv[])
     
 	bool obs_detectado = false;
     
-	auto subscription_scan = node->create_subscription<sensor_msgs::msg::LaserScan>(
-    	"/scan", 10, [&](const sensor_msgs::msg::LaserScan::SharedPtr msg) {     	 
+		auto subscription_scan = node->create_subscription<sensor_msgs::msg::LaserScan>(
+    	"/scan", 10, [&](const sensor_msgs::msg::LaserScan::SharedPtr msg) {     
+	 
+        	MinRanges(msg);
+       	 
+        	obs_detectado = false;
         	for (int i = 0; i < 18; i++) {
 			if (msg->ranges[i] < 1.0) {
 			 obs_detectado = true;
@@ -33,13 +63,20 @@ int main(int argc, char * argv[])
 
 	while (rclcpp::ok()) {
 		if (obs_detectado) {
+			if (min_range09 > min_range3509) {
+			// Girar a la izquierda
 			message_cmd_vel.linear.x = 0.0;
 			message_cmd_vel.angular.z = 0.5;
-		} else {
-			message_cmd_vel.linear.x = 0.5;
-			message_cmd_vel.angular.z = 0.0;
+			} else {
+			// Girar a la derecha
+			message_cmd_vel.linear.x = 0.0;
+			message_cmd_vel.angular.z = -0.5;
 		}
-		publisher_cmd_vel->publish(message_cmd_vel);
+   	 } else {
+   		 message_cmd_vel.linear.x = 0.5;
+   		 message_cmd_vel.angular.z = 0.0;
+   	 }
+   	 publisher_cmd_vel->publish(message_cmd_vel);
 		rclcpp::spin_some(node);
 		loop_rate.sleep();
 	}
